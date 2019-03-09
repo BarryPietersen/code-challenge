50e1f37b4eb5b439529c1f5c7272605dc4985d12
BarryPietersen <barrypietersen@hotmail.com>
2019-03-09 21:43:46 +0800

    jesse and cookies

--- HackerRank/DataStructures/Queue/Easy.cs
+++ HackerRank/DataStructures/Queue/Easy.cs
@@ -14,9 +14,9 @@ namespace HackerRank.DataStructures.Queue
             if (A.Length == 0) return -1;
             if (A.Length == 1) return A[0] >= k ? 0 : -1;
 
-            int tmp1;
-            int tmp2;
             int ops = 0;
+            int cookie1;        // 1st min at each iteration
+            int cookie2;        // 2nd min at each iteration
             Queue<int> tmpq;
             Queue<int> q1 = new Queue<int>(A.OrderBy(n => n));
             Queue<int> q2 = new Queue<int>();
@@ -24,16 +24,27 @@ namespace HackerRank.DataStructures.Queue
 
             while (q1.Count > 1 && (q1.Peek() < k || q2.Peek() < k))
             {
+                // select the two smallest cookies from the queues
+                cookie1 = q1.Peek() < q2.Peek() ? q1.Dequeue() : q2.Dequeue();
+
+                if (q2.Count == 0)
+                    cookie2 = q1.Dequeue();
+                else
+                    cookie2 = q1.Peek() < q2.Peek() ? q1.Dequeue() : q2.Dequeue();
+
+                // perform the operation on two cookies
+                // and enqueue result to 'second' queue
+                q2.Enqueue(cookie1 + (cookie2 * 2));
                 ops++;
 
-                tmp1 = q1.Peek() < q2.Peek() ? q1.Dequeue() : q2.Dequeue();
-
-                if (q2.Count == 0) tmp2 = q1.Dequeue();
-                else
-                    tmp2 = q1.Peek() < q2.Peek() ? q1.Dequeue() : q2.Dequeue();
-
-                q2.Enqueue(tmp1 + (tmp2 * 2));
-
+                // always ensure both queues have
+                // at least one cookie each, also
+                // the reason for swapping references
+                // when this condition evaluates true
+                // is to simplify the enqueuing step,
+                // and the seperation of values that
+                // have been operated on and values
+                // that are still potentially original form
                 if (q1.Count == 0)
                 {
                     tmpq = q1;
@@ -43,15 +54,16 @@ namespace HackerRank.DataStructures.Queue
                 }
                 else if (q1.Count == 1 && q2.Count > 1)
                 {
-                    int n = q1.Dequeue();
-                    q1.Enqueue(Math.Min(n, q2.Peek()));
-                    q1.Enqueue(Math.Max(n, q2.Peek()));
+                    q1.Enqueue(Math.Min(q1.Peek(), q2.Peek()));
+                    q1.Enqueue(Math.Max(q1.Peek(), q2.Peek()));
+                    q1.Dequeue();
                     q2.Dequeue();
                 }
             }
 
-            if (q1.Count > 1 || q1.Peek() >= k && q2.Peek() >= k) return ops;
+            if (q1.Peek() >= k && q2.Peek() >= k) return ops;
 
+            // check if the last two cookies in the queues can be operated on to satisfy being greater than k
             return Math.Min(q1.Peek(), q2.Peek()) + (2 * Math.Max(q1.Peek(), q2.Peek())) >= k ? ops + 1 : -1;
         }
     }
