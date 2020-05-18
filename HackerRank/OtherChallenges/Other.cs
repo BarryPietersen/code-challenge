99e746fceef2cb30ac98edd22ccb66665ce9ab2e
BarryPietersen <barrypietersen@hotmail.com>
2020-05-18 19:07:59 +1000

    sherlock and pairs problem

--- HackerRank/OtherChallenges/Other.cs
+++ HackerRank/OtherChallenges/Other.cs
@@ -905,5 +905,31 @@ namespace HackerRank
                 else curIdx = gti;
             }
         }
+
+        // https://www.hackerrank.com/challenges/sherlock-and-pairs/problem
+        static long SherlockPairs(int[] a)
+        {
+            var result = 0L;
+            var groups = new Dictionary<long, long>();
+
+            foreach (var i in a)
+            {
+                if (!groups.ContainsKey(i))
+                {
+                    groups.Add(i, 1);
+                }
+                else 
+                {
+                    groups[i]++;
+                }
+            }
+
+            foreach (var kvp in groups)
+            {
+                result += kvp.Value * (kvp.Value - 1);
+            }
+
+            return result;
+        }
     }
 }
