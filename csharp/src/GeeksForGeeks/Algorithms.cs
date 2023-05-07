b0272aa3a1cd4e55deac0f512c459efbef25d73d
BarryPietersen <barrypietersen@hotmail.com>
2023-05-07 11:02:32 +1000

    max subarray sum

--- csharp/src/GeeksForGeeks/Algorithms.cs
+++ csharp/src/GeeksForGeeks/Algorithms.cs
@@ -225,5 +225,20 @@ namespace GeeksForGeeks.OtherChallenges
 
             return -1;
         }
+
+        // https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+        public static long maxSubarraySum(int[] arr, int n)
+        {
+            long curr = arr[0];
+            long max = curr;
+
+            for (int i = 1; i < n; i++)
+            {
+                curr = Math.Max(curr + arr[i], arr[i]);
+                max = Math.Max(curr, max);
+            }
+
+            return max;
+        }
     }
 }
--- csharp/tests/GeeksForGeeks.Tests/AlgorithmsTests.cs
+++ csharp/tests/GeeksForGeeks.Tests/AlgorithmsTests.cs
@@ -23,5 +23,27 @@ namespace GeeksForGeeks.Tests
             // assert
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 0, 1 }, 1)]
+        [InlineData(new int[] { 1, 1 }, 2)]
+        [InlineData(new int[] { 1, 2, 1 }, 4)]
+        [InlineData(new int[] { 2, 0, 0, -1, 2 }, 3)]
+        [InlineData(new int[] { 1, 2, 3, -2, 5 }, 9)]
+        [InlineData(new int[] { 1, 2, 3, -7, 6 }, 6)]
+        [InlineData(new int[] { 1, 2, 3, -5, 6, 1 }, 8)]
+        [InlineData(new int[] { 1, 2, 3, -5, 6, -1, 1, 0, 10 }, 17)]
+        [InlineData(new int[] { -1, -2, -3, -4 }, -1)]
+        [InlineData(new int[] { -1, -2, -3, 0 }, 0)]
+        [InlineData(new int[] { -1, 2, -3, 0 }, 2)]
+        public void maxSubarraySum_ReturnsExpected(int[] input, int expected)
+        {
+            // act
+            var actual = Algorithms.maxSubarraySum(input, input.Length);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
